226bd90258fe538d07382b4d4d1810d34c6f3e15
Joshua Weston <bristol2801@gmail.com>
2026-06-15 11:26:15 +0100

    initial missing timestamp functions

--- updater/missing.go
+++ updater/missing.go
@@ -5,7 +5,20 @@ import (
 	"cron/internal/database"
 	"fmt"
 	"log"
+	"slices"
+	"time"
 )
+
+func GenerateWeekTimestamps() []int64 {
+	now := time.Now().Truncate(time.Hour)
+
+	timestamps := make([]int64, 24*7)
+	for i := 0; i < len(timestamps); i++ {
+		timestamps[i] = now.Add(-time.Duration(len(timestamps)-1-i) * time.Hour).Unix()
+	}
+
+	return timestamps
+}
 
 func GetMissingTimestamps(DB *database.Queries) {
 	timestamps, err := DB.GetUniqueTimestamps(context.Background())
@@ -14,5 +27,17 @@ func GetMissingTimestamps(DB *database.Queries) {
 		return
 	}
 
-	fmt.Println(timestamps)
+	allTimestamps := GenerateWeekTimestamps()
+
+	var result []int64
+
+	for _, t := range allTimestamps {
+		if !slices.Contains(timestamps, t) {
+			result = append(result, t)
+		}
+	}
+
+	fmt.Println(len(allTimestamps))
+	fmt.Println(len(timestamps))
+	fmt.Println(len(result))
 }
