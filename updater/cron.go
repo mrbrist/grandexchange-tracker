89957b078072bd04320c5622f179360a929d8027
Joshua Weston <bristol2801@gmail.com>
2026-06-18 13:50:41 +0100

    move duplicate code for fetching data into its own function

--- updater/cron.go
+++ updater/cron.go
@@ -76,72 +76,34 @@ func StartScheduling(appName string, DB *database.Queries) (*scheduler.Scheduler
 }
 
 func UpdatePriceHistoryForTimestamp(appName string, DB *database.Queries, timestamp int64) error {
-	// fmt.Println(timestamp)
-	// return nil
-
 	url := fmt.Sprintf("https://prices.runescape.wiki/api/v1/osrs/1h?timestamp=%d", timestamp)
 
-	client := &http.Client{
-		Timeout: 15 * time.Second,
-	}
-
-	fmt.Printf("Fetching %d Item Price History Data from %s...\n", timestamp, url)
-
-	req, err := http.NewRequest(http.MethodGet, url, nil)
+	err := UpdatePriceHistory(appName, DB, url)
 	if err != nil {
-		return fmt.Errorf("failed to create request: %w", err)
+		return err
 	}
 
-	// Recommended by the RuneScape Wiki API
-	req.Header.Set("User-Agent", appName)
-
-	resp, err := client.Do(req)
-	if err != nil {
-		return fmt.Errorf("failed to fetch history data: %w", err)
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return fmt.Errorf("unexpected status code: %d", resp.StatusCode)
-	}
-
-	var pricehistory1h ItemPriceHistory1h
-
-	if err := json.NewDecoder(resp.Body).Decode(&pricehistory1h); err != nil {
-		return fmt.Errorf("failed to decode response: %w", err)
-	}
-
-	for id, item := range pricehistory1h.Data {
-		id, err := strconv.Atoi(id)
-		if err != nil {
-			fmt.Println(err)
-		}
-		err = DB.AddItemHistory(context.Background(), database.AddItemHistoryParams{
-			ItemID:         int32(id),
-			PriceTimestamp: pricehistory1h.Timestamp,
-			AvgHighPrice:   int32(item.AvgHighPrice),
-			AvgLowPrice:    int32(item.AvgLowPrice),
-			HighVolume:     int64(item.HighPriceVolume),
-			LowVolume:      int64(item.LowPriceVolume),
-		})
-		if err != nil {
-			// Disable error from flooding terminal since its always duplicate error
-			// fmt.Printf("failed to add history to database: %s", err)
-			continue
-		}
-	}
-	fmt.Printf("%sAdded new data to database with timestamp: %d%s\n", ColorPurple, pricehistory1h.Timestamp, ColorNone)
 	return nil
 }
 
 func UpdatePriceHistory1h(appName string, DB *database.Queries) error {
 	url := "https://prices.runescape.wiki/api/v1/osrs/1h"
 
+	err := UpdatePriceHistory(appName, DB, url)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("%swaiting for next update in 1h...%s\n", ColorGreen, ColorNone)
+	return nil
+}
+
+func UpdatePriceHistory(appName string, DB *database.Queries, url string) error {
 	client := &http.Client{
 		Timeout: 15 * time.Second,
 	}
 
-	fmt.Printf("Fetching Item Price History 1h Data from %s...\n", url)
+	fmt.Printf("Fetching Item Price History Data from %s...\n", url)
 
 	req, err := http.NewRequest(http.MethodGet, url, nil)
 	if err != nil {
@@ -187,6 +149,5 @@ func UpdatePriceHistory1h(appName string, DB *database.Queries) error {
 		}
 	}
 	fmt.Printf("%sAdded new data to database with timestamp: %d%s\n", ColorPurple, pricehistory1h.Timestamp, ColorNone)
-	fmt.Printf("%swaiting for next update in 1h...%s\n", ColorGreen, ColorNone)
 	return nil
 }
